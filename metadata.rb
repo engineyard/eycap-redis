89f4f1d8075b6f79710bf624e5b7368b44f71e72
Tyler Bird <tyler.bird@gmail.com>
2014-10-11 08:55:00 -0600

    rename cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name              "redis-tlq"
-maintainer        "Ben Dixon"
-maintainer_email  "ben@talkingquickly.co.uk"
+name              "eycap-redis"
+maintainer        "Ben Dixon, Tyler Bird"
+maintainer_email  "ben@talkingquickly.co.uk, tbird@engineyard.com"
 description       "Installs redis from rwky's ppa"
 version           "0.0.6"
 
-recipe "redis-tlq", "Installs redis"
+recipe "eycap-redis", "Installs redis"
 
 supports "ubuntu"
